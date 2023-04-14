8f04e9a1eaec3c1baa773c6dd9f14199116e0396
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-04-15 00:13:34 +0800

    feat: only send one webhook

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ import { getRooResets } from './roo/schedule/reset';
 import { trades } from './roo/schedule/trade';
 
 import { DiscordWebhookEmbed, DiscordWebhookPayload } from './types';
-import { toSpaceSeparatedPascalCase } from './utilities';
 
 const scheduled = ((_controller, env, ctx) => {
 	const date = utcToZonedTime(Date.now(), ROO_TIME_ZONE);
@@ -23,38 +22,33 @@ const scheduled = ((_controller, env, ctx) => {
 		...trades.map((value): RooSchedule => [value, RooScheduleKind.Trade]),
 	] satisfies RooSchedule[];
 
-	const embeds = [] as [DiscordWebhookEmbed, RooScheduleKind][];
+	const embedsWithKind = [] as [DiscordWebhookEmbed, RooScheduleKind][];
 	for (const schedule of schedules) {
 		const time = getScheduleTime(schedule);
 		const match = matchEvent(time, date);
 		if (match !== undefined) {
 			const embed = generateEmbed(schedule, match, date);
-			embeds.push([embed, schedule[1]]);
+			embedsWithKind.push([embed, schedule[1]]);
 		}
 	}
 
-	if (embeds.length > 0) {
+	if (embedsWithKind.length > 0) {
 		// <@&{ID}> is role mention
 		// ref:  https://discord.com/developers/docs/reference#message-formatting-formats
 		const mention = `<@&${env.DISCORD_ROLE_MENTION_ID}>`;
+		const kinds = [...new Set(embedsWithKind.map(([, kind]) => RooScheduleKind[kind]))].join(' & ');
+
+		const payload = {
+			content: `${mention} ${kinds}`,
+			embeds: embedsWithKind.map(([embed]) => embed),
+		} satisfies DiscordWebhookPayload;
 
 		ctx.waitUntil(
-			Promise.all(
-				embeds
-					.map(
-						([embed, kind]): DiscordWebhookPayload => ({
-							content: `${mention} ${toSpaceSeparatedPascalCase(RooScheduleKind[kind])}`,
-							embeds: [embed],
-						}),
-					)
-					.map((payload) =>
-						fetch(env.DISCORD_WEBHOOK_URL, {
-							body: JSON.stringify(payload),
-							headers: { 'Content-Type': 'application/json' },
-							method: 'POST',
-						}),
-					),
-			),
+			fetch(env.DISCORD_WEBHOOK_URL, {
+				body: JSON.stringify(payload),
+				headers: { 'Content-Type': 'application/json' },
+				method: 'POST',
+			}),
 		);
 	}
 }) satisfies ExportedHandlerScheduledHandler<Env>;
