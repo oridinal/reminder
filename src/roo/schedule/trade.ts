497dae7d260781293e23244e4f8d31d0161b2182
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-04-29 18:36:17 +0800

    feat: remove auction reminder
    I don't really use them

--- src/roo/schedule/trade.ts
+++ src/roo/schedule/trade.ts
@@ -1,39 +1,23 @@
 import { ScheduleTime } from '.';
 
 export enum Trade {
-	AuctionConclude,
-	AuctionRefresh,
-	AuctionStart,
 	FirstStoreRefresh,
 	SecondStoreRefresh,
 	ThirdStoreRefresh,
 }
 
-export const trades = [
-	Trade.AuctionConclude,
-	Trade.AuctionRefresh,
-	Trade.AuctionStart,
-	Trade.FirstStoreRefresh,
-	Trade.SecondStoreRefresh,
-	Trade.ThirdStoreRefresh,
-];
+export const trades = [Trade.FirstStoreRefresh, Trade.SecondStoreRefresh, Trade.ThirdStoreRefresh];
 
 export const getTradeTime = (value: Trade): ScheduleTime => {
 	switch (value) {
-		case Trade.AuctionRefresh:
-			return { hours: 5, minutes: 0 };
-
 		case Trade.FirstStoreRefresh:
 			return { hours: 12, minutes: 0 };
 
 		case Trade.SecondStoreRefresh:
 			return { hours: 16, minutes: 0 };
 
-		case Trade.AuctionStart:
+		// case Trade.AuctionStart:
 		case Trade.ThirdStoreRefresh:
 			return { hours: 20, minutes: 0 };
-
-		case Trade.AuctionConclude:
-			return { hours: 22, minutes: 0 };
 	}
 };
