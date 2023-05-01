cec6dd7b4b5950151c1833e42f5f69334e8837fd
Ahmad Yasser <99479536+aynh@users.noreply.github.com>
2023-05-01 23:26:16 +0800

    fix: Date's month starts from 0

--- src/roo/schedule/event.ts
+++ src/roo/schedule/event.ts
@@ -10,16 +10,16 @@ export const getEvents = (date: Date): Event[] => {
 	return (
 		[
 			[
+				Event.VeinsStrategicBattle, // (4/27 5:00 - 5/4 4:59)
 				{
-					start: set(date, { year: 2023, month: 4, date: 27, hours: 5, minutes: 0, seconds: 0, milliseconds: 0 }),
-					end: set(date, { year: 2023, month: 5, date: 4, hours: 4, minutes: 59, seconds: 59, milliseconds: 59 }),
+					start: set(date, { year: 2023, month: 3, date: 27, hours: 5, minutes: 0, seconds: 0, milliseconds: 0 }),
+					end: set(date, { year: 2023, month: 4, date: 4, hours: 4, minutes: 59, seconds: 59, milliseconds: 59 }),
 				},
-				Event.VeinsStrategicBattle,
 			],
-		] satisfies [Interval, Event][]
+		] satisfies [Event, Interval][]
 	)
-		.filter(([interval]) => isWithinInterval(date, interval))
-		.map(([, event]) => event);
+		.filter(([, interval]) => isWithinInterval(date, interval))
+		.map(([event]) => event);
 };
 
 export const getEventTime = (value: Event): ScheduleTime => {
